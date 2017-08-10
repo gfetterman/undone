c8b335fa8fe4918d2358b197f7c781080369a2d3
Graham Fetterman <graham.fetterman@gmail.com>
2017-08-10 02:21:32 -0500

    Refactor test code

--- tests/test_undone.py
+++ tests/test_undone.py
@@ -3,130 +3,109 @@ import functools
 import pytest
 import undone
 
+INITIAL_STATE = [4]
+SECOND_STATE = [4, 1]
+THIRD_STATE = [4, 1, 1]
+FOURTH_STATE = [4, 1, 2]
+FIFTH_STATE = ['totally', 'new', 'list']
+SIXTH_STATE = [4, 64, 256]
+
+def common_tests(stack, resource, ops):
+    stack.do(ops[0])
+    assert resource() == SECOND_STATE
+    stack.do(ops[1])
+    assert resource() == THIRD_STATE
+    stack.undo()
+    assert resource() == SECOND_STATE
+    stack.undo()
+    assert resource() == INITIAL_STATE
+    with pytest.raises(IndexError):
+        stack.undo()
+    stack.redo()
+    assert resource() == SECOND_STATE
+    stack.redo()
+    assert resource() == THIRD_STATE
+    with pytest.raises(IndexError):
+        stack.redo()
+    stack.undo()
+    assert stack.undone == [ops[1]]
+    stack.do(ops[2])
+    assert resource() == FOURTH_STATE
+    assert stack.undone == []
+
 def test_reversible():
-    resource = [4]
-    def add_one(r):
-        r.append(1)
+    resource = []
+    resource.extend(INITIAL_STATE)
     def remove_one(r):
         try:
             r.pop()
         except IndexError:
             pass
     Revop = collections.namedtuple("Revop", ["do", "undo"])
-    adding_one = Revop(functools.partial(add_one, r=resource),
+    adding_one = Revop(functools.partial(lambda r: r.append(1), r=resource),
                        functools.partial(remove_one, r=resource))
-    stack = undone.ReversibleUndoStack()
-    stack.do(adding_one)
-    assert resource == [4 ,1]
-    stack.do(adding_one)
-    assert resource == [4, 1, 1]
-    stack.undo()
-    assert resource == [4, 1]
-    stack.undo()
-    assert resource == [4]
-    with pytest.raises(IndexError):
-        stack.undo()
-    stack.redo()
-    assert resource == [4, 1]
-    stack.redo()
-    assert resource == [4, 1, 1]
-    with pytest.raises(IndexError):
-        stack.redo()
-    stack.undo()
-    assert resource == [4, 1]
-    assert stack.done == [adding_one]
-    assert stack.undone == [adding_one]
     adding_two = Revop(functools.partial(lambda r: r.append(2), r=resource),
                        functools.partial(remove_one, r=resource))
-    stack.do(adding_two)
-    assert resource == [4, 1, 2]
-    assert stack.done == [adding_one, adding_two]
-    assert stack.undone == []
+    stack = undone.ReversibleUndoStack()
+    common_tests(stack,
+                 lambda: resource,
+                 [adding_one, adding_one, adding_two])
 
 def test_regenerating():
-    resource = [4]
     def add_one(r):
         r.append(1)
-    stack = undone.RegeneratingUndoStack(initial_state=resource)
-    stack.do(add_one)
-    assert stack.current_state == [4 ,1]
-    # if resource is a mutable object, resource and current_state become
-    # decoupled
-    assert resource == [4]
-    stack.do(add_one)
-    assert stack.current_state == [4, 1, 1]
-    stack.undo()
-    assert stack.current_state == [4, 1]
-    stack.undo()
-    assert stack.current_state == [4]
-    with pytest.raises(IndexError):
-        stack.undo()
-    stack.redo()
-    assert stack.current_state == [4, 1]
-    stack.redo()
-    assert stack.current_state == [4, 1, 1]
-    with pytest.raises(IndexError):
-        stack.redo()
-    stack.undo()
-    assert stack.current_state == [4, 1]
-    assert stack.done == [add_one]
-    assert stack.undone == [add_one]
     def add_two(r):
         r.append(2)
-    stack.do(add_two)
-    assert stack.current_state == [4, 1, 2]
-    assert stack.done == [add_one, add_two]
-    assert stack.undone == []
+    stack = undone.RegeneratingUndoStack(initial_state=INITIAL_STATE)
+    def resource(s):
+        return s.current_state
+    common_tests(stack,
+                 functools.partial(resource, s=stack),
+                 [add_one, add_one, add_two])
 
 def test_snapshot():
-    resource = [4]
-    stack = undone.SnapshotUndoStack(resource)
-    assert stack.initial_state == [4]
-    assert stack.current_state == [4]
+    stack = undone.SnapshotUndoStack(INITIAL_STATE)
+    assert stack.current_state == INITIAL_STATE
     assert not stack.snapshots
     assert not stack.forward_snapshots
-    stack.current_state.append(16)
-    assert stack.initial_state == [4]
-    assert stack.current_state == [4, 16]
+    stack.current_state.append(1)
+    assert stack.current_state == SECOND_STATE
     assert not stack.snapshots
     assert not stack.forward_snapshots
     stack.do()
-    assert stack.initial_state == [4]
-    assert stack.current_state == [4, 16]
-    assert stack.snapshots == [[4, 16]]
-    assert not stack.forward_snapshots
-    stack.current_state = ['totally', 'new', 'list']
+    assert stack.current_state == SECOND_STATE
+    assert stack.snapshots == [SECOND_STATE]
+    stack.current_state = FIFTH_STATE
     stack.do()
-    assert stack.initial_state == [4]
-    assert stack.current_state == ['totally', 'new', 'list']
-    assert stack.snapshots[-1] == ['totally', 'new', 'list']
-    assert not stack.forward_snapshots
+    assert stack.current_state == FIFTH_STATE
+    assert stack.snapshots[-1] == FIFTH_STATE
     stack.undo()
-    assert stack.current_state == [4, 16]
-    assert stack.snapshots == [[4, 16]]
-    assert stack.forward_snapshots == [['totally', 'new', 'list']]
+    assert stack.current_state == SECOND_STATE
+    assert stack.snapshots == [SECOND_STATE]
+    assert stack.forward_snapshots == [FIFTH_STATE]
     stack.undo()
-    assert stack.current_state == [4]
+    assert stack.current_state == INITIAL_STATE
     assert not stack.snapshots
-    assert stack.forward_snapshots[-1] == [4, 16]
+    assert stack.forward_snapshots[-1] == SECOND_STATE
     with pytest.raises(IndexError):
         stack.undo()
     stack.redo()
-    assert stack.current_state == [4, 16]
-    assert stack.snapshots == [[4, 16]]
-    assert stack.forward_snapshots == [['totally', 'new', 'list']]
+    assert stack.current_state == SECOND_STATE
+    assert stack.snapshots == [SECOND_STATE]
+    assert stack.forward_snapshots == [FIFTH_STATE]
     stack.redo()
-    assert stack.current_state == ['totally', 'new', 'list']
-    assert stack.snapshots == [[4, 16], ['totally', 'new', 'list']]
+    assert stack.current_state == FIFTH_STATE
+    assert stack.snapshots == [SECOND_STATE, FIFTH_STATE]
     assert not stack.forward_snapshots
     with pytest.raises(IndexError):
         stack.redo()
     stack.undo()
-    assert stack.current_state == [4, 16]
-    assert stack.snapshots == [[4, 16]]
-    assert stack.forward_snapshots == [['totally', 'new', 'list']]
-    stack.current_state.append(64)
+    assert stack.current_state == SECOND_STATE
+    assert stack.snapshots == [SECOND_STATE]
+    assert stack.forward_snapshots == [FIFTH_STATE]
+    stack.current_state.extend([64, 256])
+    del stack.current_state[1]
     stack.do()
-    assert stack.current_state == [4, 16, 64]
-    assert stack.snapshots == [[4, 16], [4, 16, 64]]
-    assert stack.forward_snapshots == []
+    assert stack.current_state == SIXTH_STATE
+    assert stack.snapshots == [SECOND_STATE, SIXTH_STATE]
+    assert not stack.forward_snapshots
