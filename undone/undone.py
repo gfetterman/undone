93dad06364141b2c029805fd5225afa93da772b6
Graham Fetterman <graham.fetterman@gmail.com>
2017-08-13 19:34:37 -0500

    Add method docstrings

--- undone/undone.py
+++ undone/undone.py
@@ -33,16 +33,35 @@ class ReversibleUndoStack(UndoStack):
         self.undone = []
     
     def do(self, op):
+        """Adds `op` to undo stack and applies it.
+        
+        As the redo stack is now invalid, also erases it.
+        
+        Arguments:
+            op: operation object to apply; must have "do" and "redo" methods
+        """
         self.done.append(op)
         self.undone = []
         return op.do()
     
     def undo(self):
+        """Reverts the last undo stack operation and adds to redo stack.
+        
+        Does this by applying the "undo" method of the operation.
+        
+        Raises:
+            IndexError: if the undo stack is empty.
+        """
         op = self.done.pop()
         self.undone.append(op)
         return op.undo()
     
     def redo(self):
+        """Applies top redo stack operation and adds to undo stack.
+        
+        Raises:
+            IndexError: if the redo stack is empty.
+        """
         op = self.undone.pop()
         self.done.append(op)
         return op.do()
@@ -69,21 +88,46 @@ class RegeneratingUndoStack(UndoStack):
     through this undo stack object, via its `current_state` variable.
     """
     def __init__(self, initial_state):
+        """Creates a new RegeneratingUndoStack.
+        
+        Arguments:
+            initial_state: objects to apply operations to
+        """
         self.current_state = copy.deepcopy(initial_state)
         self.initial_state = copy.deepcopy(initial_state)
         self.done = []
         self.undone = []
     
     def do(self, op):
+        """Adds `op` to undo stack and applies it.
+        
+        As the redo stack is now invalid, also erases it.
+        
+        Arguments:
+            op (callable): operation to apply; must take 1 positional argument
+        """
         self.done.append(op)
         self.undone = []
         op(self.current_state)
     
     def undo(self):
+        """Reverts the last undo stack operation and adds to redo stack.
+        
+        Does this by re-applying all but the last undo stack operation to the
+        initial state.
+        
+        Raises:
+            IndexError: if the undo stack is empty.
+        """
         self.undone.append(self.done.pop())
         self._regenerate()
     
     def redo(self):
+        """Applies top redo stack operation and adds to undo stack.
+        
+        Raises:
+            IndexError: if the redo stack is empty.
+        """
         op = self.undone.pop()
         self.done.append(op)
         op(self.current_state)
@@ -108,18 +152,41 @@ class SnapshotUndoStack(UndoStack):
     through this undo stack object, via its `current_state` variable.
     """
     def __init__(self, objects):
+        """Creates a new SnapshotUndoStack object.
+        
+        Arguments:
+            objects: object or objects to take snapshots of
+        """
         self.snapshots = []
         self.forward_snapshots = []
         self.current_state = copy.deepcopy(objects)
         self.initial_state = copy.deepcopy(objects)
     
     def do(self, new_state=None):
+        """Take a snapshot of some objects and add it to the undo stack.
+        
+        If `new_state` is given, substitutes it for `current_state` and takes
+        snapshot. Otherwise, the snapshot is of the existing `current_state`.
+        
+        Because the redo stack is now (conceptually) invalid, erases it.
+        
+        Arguments:
+            new_state: if present, replaces `current_state`
+        """
         if new_state is not None:
             self.current_state = new_state
         self.snapshots.append(copy.deepcopy(self.current_state))
         self.forward_snapshots = []
     
     def undo(self):
+        """Adds the last snapshot to redo stack and reverts to the one before.
+        
+        If current_state contains changes not in the last snapshot, those are
+        discarded too.
+        
+        Raises:
+            IndexError: if the undo stack is empty
+        """
         self.forward_snapshots.append(self.snapshots.pop())
         if self.snapshots:
             self.current_state = copy.deepcopy(self.snapshots[-1])
@@ -127,17 +194,31 @@ class SnapshotUndoStack(UndoStack):
             self.current_state = copy.deepcopy(self.initial_state)
     
     def redo(self):
+        """Adds the next snapshot to the undo stack and assumes its values.
+        
+        If current_state contains changes not in the last snapshot, they are
+        lost.
+        
+        Raises:
+            IndexError: if the redo stack is empty.
+        """
         self.snapshots.append(self.forward_snapshots.pop())
         self.current_state = copy.deepcopy(self.snapshots[-1])
     
     @property
     def clean(self):
+        """Checks whether `current_state` matches the last snapshot.
+        
+        Returns:
+            bool: whether `current_state` matches the last snapshot.
+        """
         if self.snapshots:
             return self.current_state == self.snapshots[-1]
         else:
             return self.current_state == self.initial_state
     
     def discard_changes(self):
+        """Discards any unrecorded changes to `current_state`."""
         if not self.clean:
             if self.snapshots:
                 self.current_state = copy.deepcopy(self.snapshots[-1])
